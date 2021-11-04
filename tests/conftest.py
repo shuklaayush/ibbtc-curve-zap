c9a230d45dad7a8618dda89895ecf8313d822e04
Ayush Shukla <shuklaayush247@gmail.com>
2021-11-04 23:34:34 +0530

    refactor: move intialization to conftest

---
+++ tests/conftest.py
@@ -0,0 +1,99 @@
+import pytest
+
+MAX_UINT256 = 2 ** 256 - 1
+
+
+@pytest.fixture(scope="session")
+def weth(interface):
+    yield interface.ERC20("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2")
+
+
+@pytest.fixture(scope="session")
+def wbtc(interface):
+    yield interface.ERC20("0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599")
+
+
+@pytest.fixture(scope="session")
+def renbtc(interface):
+    yield interface.ERC20("0xEB4C2781e4ebA804CE9a9803C67d0893436bB27D")
+
+
+@pytest.fixture(scope="session")
+def sbtc(interface):
+    yield interface.ERC20("0xfE18be6b3Bd88A2D2A7f928d00292E7a9963CfC6")
+
+
+@pytest.fixture(scope="session")
+def ibbtc(Contract):
+    yield Contract("0xc4E15973E6fF2A35cC804c2CF9D2a1b817a8b40F")
+
+
+@pytest.fixture(scope="session")
+def wibbtc(Contract):
+    yield Contract("0x8751D4196027d4e6DA63716fA7786B5174F04C15")
+
+
+@pytest.fixture(scope="session")
+def sbtc_pool(Contract):
+    yield Contract("0x7fC77b5c7614E1533320Ea6DDc2Eb61fa00A9714")
+
+
+@pytest.fixture(scope="session")
+def sbtcCrv(Contract):
+    yield Contract("0x075b1bb99792c9E1041bA13afEf80C91a1e70fB3")
+
+
+@pytest.fixture(scope="session")
+def router(Contract):
+    yield Contract("0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F")
+
+
+@pytest.fixture(scope="session")
+def deployer(accounts):
+    yield accounts[0]
+
+
+@pytest.fixture(autouse=True)
+def ibbtc_zap(DepositZapibBTC, deployer):
+    yield DepositZapibBTC.deploy({"from": deployer})
+
+
+@pytest.fixture(autouse=True)
+def get_tokens(deployer, router, weth, ibbtc, wbtc, sbtc_pool):
+    # Get ibbtc
+    router.swapETHForExactTokens(
+        2 * 10 ** ibbtc.decimals(),
+        [weth, wbtc, ibbtc],
+        deployer,
+        MAX_UINT256,
+        {"from": deployer, "value": deployer.balance()},
+    )
+
+    # Get wbtc
+    router.swapETHForExactTokens(
+        3 * 10 ** wbtc.decimals(),
+        [weth, wbtc],
+        deployer,
+        MAX_UINT256,
+        {"from": deployer, "value": deployer.balance()},
+    )
+    # Get renbtc, sbtc from curve sbtc pool
+    wbtc.approve(sbtc_pool, MAX_UINT256, {"from": deployer})
+    # renbtc
+    sbtc_pool.exchange(1, 0, 10 ** wbtc.decimals(), 0, {"from": deployer})
+    # sbtc
+    sbtc_pool.exchange(1, 2, 10 ** wbtc.decimals(), 0, {"from": deployer})
+
+
+@pytest.fixture(autouse=True)
+def set_approvals(deployer, ibbtc, wbtc, renbtc, sbtc, ibbtc_zap):
+    # Approvals
+    ibbtc.approve(ibbtc_zap, MAX_UINT256, {"from": deployer})
+    renbtc.approve(ibbtc_zap, MAX_UINT256, {"from": deployer})
+    wbtc.approve(ibbtc_zap, MAX_UINT256, {"from": deployer})
+    sbtc.approve(ibbtc_zap, MAX_UINT256, {"from": deployer})
+
+
+@pytest.fixture(autouse=True)
+def isolation(fn_isolation):
+    pass
--- tests/test_zap.py
+++ tests/test_zap.py
@@ -1,299 +1,201 @@
-import time
-
-from brownie import *
-import brownie
-
-from rich.console import Console
-
-console = Console()
-
-from dotmap import DotMap
 import pytest
+from pytest import approx
 
-def test_zap_flow(deployer, rando, metapool, ibbtc_zap, ibbtc, renbtc, wBTC, sBTC, wibBTC, lp_token):
-
-    initialize(deployer, ibbtc_zap, metapool, ibbtc, renbtc, wBTC, sBTC)
-
-    approve_rando(rando, ibbtc_zap, metapool, ibbtc, renbtc, wBTC, sBTC)
-
-    ibbtc.approve(rando, 9999999999999999999999, {"from": deployer})
-
-    balance_ibbtc_deployer = ibbtc.balanceOf(deployer)
-    ibbtc.transfer(rando, balance_ibbtc_deployer // 2, {"from": deployer})
-
-    balance_ibbtc_rando_before = ibbtc.balanceOf(rando)
-    balance_metapool_rando = metapool.balanceOf(rando)
-
-    # deposit ibbtc from rando
-    amounts = [balance_ibbtc_rando_before, 0, 0, 0]
-    ibbtc_zap.add_liquidity(metapool.address, amounts, 0, {"from": rando})
-
-    # remove liquidity in terms of ibbtc
-    ibbtc_zap.remove_liquidity_one_coin(metapool, metapool.balanceOf(rando), 0, 0, {"from": rando})
-
-    balance_ibbtc_rando_after = ibbtc.balanceOf(rando)
-
-    assert balance_ibbtc_rando_after >= balance_ibbtc_rando_before * 0.99
+from conftest import MAX_UINT256
 
 
-# Test to add liquidity to metapool
-def test_add_liquidity(deployer, metapool, ibbtc_zap, ibbtc, renbtc, wBTC, sBTC, wibBTC, lp_token):
-    
-    initialize(deployer, ibbtc_zap, metapool, ibbtc, renbtc, wBTC, sBTC)
-    
+@pytest.fixture(scope="module")
+def ibbtc_pool(Contract):
+    yield Contract("0xFbdCA68601f835b27790D98bbb8eC7f05FDEaA9B")
+
+
+@pytest.fixture(autouse=True)
+def seed_pool(wibbtc, ibbtc_zap, ibbtc_pool, deployer, renbtc, wbtc, sbtc):
+    ibbtc_pool.approve(ibbtc_zap, MAX_UINT256, {"from": deployer})
+
+    # Initialize pool to 1: 1 wibbtc/sbtcCrv peg
+    amounts = [
+        wibbtc.balanceToShares(10 ** wibbtc.decimals()),  # ibbtc
+        10 ** renbtc.decimals() // 3,  # renbtc
+        10 ** wbtc.decimals() // 3,  # wbtc
+        10 ** sbtc.decimals() // 3,  # sbtc
+    ]
+    ibbtc_zap.add_liquidity(ibbtc_pool, amounts, 0, {"from": deployer})
+
+
+# Test to add liquidity to ibbtc pool
+def test_add_liquidity(deployer, ibbtc_pool, ibbtc_zap, ibbtc, renbtc, wibbtc, sbtcCrv):
     # add ibbtc liquidity
     amounts = [ibbtc.balanceOf(deployer) // 10, 0, 0, 0]
 
-    wibBTC_shares_before = wibBTC.sharesOf(metapool)
-    ibbtc_zap.add_liquidity(metapool.address, amounts, 0, {"from": deployer})
-    wibBTC_shares_after = wibBTC.sharesOf(metapool)
+    wibbtc_shares_before = wibbtc.sharesOf(ibbtc_pool)
+    ibbtc_zap.add_liquidity(ibbtc_pool, amounts, 0, {"from": deployer})
+    wibbtc_shares_after = wibbtc.sharesOf(ibbtc_pool)
 
     # depositing ibbtc into curve pool, wibbtc shares of curve pool should increase propotionally to amount of ibbtc shares deposited
-    assert wibBTC_shares_after - wibBTC_shares_before >= amounts[0] * 0.99 # multiplying by 0.99 for slippage considerations
+    assert float(wibbtc_shares_after - wibbtc_shares_before) == approx(
+        amounts[0], rel=1e-2
+    )
 
     # add renbtc liquidity
-    # when depositing renbtc/wBTC/sBTC the balance of lp token will increase
+    # when depositing renbtc/wbtc/sbtc the balance of lp token will increase
 
     amounts = [0, renbtc.balanceOf(deployer) // 10, 0, 0]
 
-    lp_balance_before = lp_token.balanceOf(metapool)
-    ibbtc_zap.add_liquidity(metapool.address, amounts, 0, {"from": deployer})
-    lp_balance_after = lp_token.balanceOf(metapool)
-    
-    assert lp_balance_after > lp_balance_before
+    sbtcCrv_before = sbtcCrv.balanceOf(ibbtc_pool)
+    ibbtc_zap.add_liquidity(ibbtc_pool, amounts, 0, {"from": deployer})
+    sbtcCrv_after = sbtcCrv.balanceOf(ibbtc_pool)
+
+    assert sbtcCrv_after > sbtcCrv_before
 
 
-# Test to remove liquidity from metapool in equal proportions
-def test_remove_liquidity(deployer, metapool, ibbtc_zap, ibbtc, renbtc, wBTC, sBTC, wibBTC, lp_token):
-
-    initialize(deployer, ibbtc_zap, metapool, ibbtc, renbtc, wBTC, sBTC)
-    
-    balance = metapool.balanceOf(deployer)
+# Test to remove liquidity from ibbtc pool in equal proportions
+def test_remove_liquidity(
+    deployer, ibbtc_pool, ibbtc_zap, ibbtc, renbtc, wbtc, sbtc, wibbtc, sbtcCrv
+):
     min_amounts = [0] * 4
-    
+
     balance_ibbtc_before = ibbtc.balanceOf(deployer)
     balance_renbtc_before = renbtc.balanceOf(deployer)
-    balance_wBTC_before = wBTC.balanceOf(deployer)
-    balance_sBTC_before = sBTC.balanceOf(deployer)
-    balance_wibbtc_before_metapool = wibBTC.balanceOf(metapool)
-    lp_balance_before = lp_token.balanceOf(metapool)
+    balance_wbtc_before = wbtc.balanceOf(deployer)
+    balance_sbtc_before = sbtc.balanceOf(deployer)
+    balance_wibbtc_before_ibbtc_pool = wibbtc.balanceOf(ibbtc_pool)
+    balance_sbtcCrv_before = sbtcCrv.balanceOf(ibbtc_pool)
 
-    ibbtc_zap.remove_liquidity(metapool.address, balance // 10, min_amounts, {"from": deployer})
-    
+    ibbtc_zap.remove_liquidity(
+        ibbtc_pool,
+        ibbtc_pool.balanceOf(deployer) // 10,
+        min_amounts,
+        {"from": deployer},
+    )
+
     balance_ibbtc_after = ibbtc.balanceOf(deployer)
     balance_renbtc_after = renbtc.balanceOf(deployer)
-    balance_wBTC_after = wBTC.balanceOf(deployer)
-    balance_sBTC_after = sBTC.balanceOf(deployer)
-    balance_wibbtc_after_metapool = wibBTC.balanceOf(metapool)
-    lp_balance_after = lp_token.balanceOf(metapool)
+    balance_wbtc_after = wbtc.balanceOf(deployer)
+    balance_sbtc_after = sbtc.balanceOf(deployer)
+    balance_wibbtc_after_ibbtc_pool = wibbtc.balanceOf(ibbtc_pool)
+    balance_sbtcCrv_after = sbtcCrv.balanceOf(ibbtc_pool)
 
-    # deployer ibbtc, renbtc, wBTC, sBTC balance should increase
+    # deployer ibbtc, renbtc, wbtc, sbtc balance should increase
     assert balance_ibbtc_after > balance_ibbtc_before
     assert balance_renbtc_after > balance_renbtc_before
-    assert balance_wBTC_after > balance_wBTC_before
-    assert balance_sBTC_after > balance_sBTC_before
+    assert balance_wbtc_after > balance_wbtc_before
+    assert balance_sbtc_after > balance_sbtc_before
 
-    # metapool wibbtc balance should decrease
-    assert balance_wibbtc_before_metapool > balance_wibbtc_after_metapool
+    # ibbtc_pool wibbtc balance should decrease
+    assert balance_wibbtc_before_ibbtc_pool > balance_wibbtc_after_ibbtc_pool
 
-    # metapool lptoken balance should decrease
-    assert lp_balance_after < lp_balance_before
+    # ibbtc_pool lptoken balance should decrease
+    assert balance_sbtcCrv_after < balance_sbtcCrv_before
 
 
-# Test to remove liquidity from metapool by removing a single coin
-def test_remove_one_coin(deployer, metapool, ibbtc_zap, ibbtc, renbtc, wBTC, sBTC, wibBTC):
+# Test to remove liquidity from ibbtc pool by removing a single coin
+def test_remove_one_coin(deployer, ibbtc_pool, ibbtc_zap, ibbtc, renbtc, wbtc, sbtc):
+    balance = ibbtc_pool.balanceOf(deployer)
 
-    initialize(deployer, ibbtc_zap, metapool, ibbtc, renbtc, wBTC, sBTC)
-
-    balance = metapool.balanceOf(deployer)
-    
+    # remove ibbtc
     amount = balance // 4
     before_ibbtc_balance = ibbtc.balanceOf(deployer)
-    ibbtc_zap.remove_liquidity_one_coin(metapool, amount, 0, 0, {"from": deployer})
+    ibbtc_zap.remove_liquidity_one_coin(ibbtc_pool, amount, 0, 0, {"from": deployer})
     after_ibbtc_balance = ibbtc.balanceOf(deployer)
     assert after_ibbtc_balance > before_ibbtc_balance
 
+    # remove renbtc
     amount = balance // 4
     before_renbtc_balance = renbtc.balanceOf(deployer)
-    ibbtc_zap.remove_liquidity_one_coin(metapool, amount, 1, 0, {"from": deployer})
+    ibbtc_zap.remove_liquidity_one_coin(ibbtc_pool, amount, 1, 0, {"from": deployer})
     after_renbtc_balance = renbtc.balanceOf(deployer)
     assert after_renbtc_balance > before_renbtc_balance
 
+    # remove wbtc
     amount = balance // 4
-    before_wbtc_balance = wBTC.balanceOf(deployer)
-    ibbtc_zap.remove_liquidity_one_coin(metapool, amount, 2, 0, {"from": deployer})
-    after_wbtc_balance = wBTC.balanceOf(deployer)
+    before_wbtc_balance = wbtc.balanceOf(deployer)
+    ibbtc_zap.remove_liquidity_one_coin(ibbtc_pool, amount, 2, 0, {"from": deployer})
+    after_wbtc_balance = wbtc.balanceOf(deployer)
     assert after_wbtc_balance > before_wbtc_balance
+
+    # remove sbtc
+    amount = balance // 4
+    before_sbtc_balance = sbtc.balanceOf(deployer)
+    ibbtc_zap.remove_liquidity_one_coin(ibbtc_pool, amount, 3, 0, {"from": deployer})
+    after_sbtc_balance = sbtc.balanceOf(deployer)
+    assert after_sbtc_balance > before_sbtc_balance
+
+
+# Testing calc_withdraw_one_coin
+def test_calc_withdraw_one_coin(deployer, ibbtc_pool, ibbtc_zap, ibbtc):
+    amount = ibbtc_pool.balanceOf(deployer) // 10
+    withdraw_amount_ibbtc = ibbtc_zap.calc_withdraw_one_coin(
+        ibbtc_pool, amount, 0, {"from": deployer}
+    )
+
+    before_ibbtc_shares = ibbtc.balanceOf(deployer)
+    ibbtc_zap.remove_liquidity_one_coin(ibbtc_pool, amount, 0, 0, {"from": deployer})
+    after_ibbtc_shares = ibbtc.balanceOf(deployer)
+
+    assert float(after_ibbtc_shares - before_ibbtc_shares) == approx(
+        withdraw_amount_ibbtc, rel=1e-2
+    )
 
 
 # Testing swap between different pairs
-def test_swap(deployer, metapool, ibbtc_zap, ibbtc, renbtc, wBTC, sBTC):
-    
-    initialize(deployer, ibbtc_zap, metapool, ibbtc, renbtc, wBTC, sBTC)
-
+def test_swap(deployer, ibbtc_pool, ibbtc_zap, ibbtc, renbtc, wbtc):
     # swap ibbtc -> wbtc
     ibbtc_amount = ibbtc.balanceOf(deployer) // 10
 
-    balance_wbtc_before_swap = wBTC.balanceOf(deployer)
-    ibbtc_zap.exchange_underlying(metapool, 0, 2, ibbtc_amount, 0)
-    balance_wbtc_after_swap = wBTC.balanceOf(deployer)
+    balance_wbtc_before_swap = wbtc.balanceOf(deployer)
+    ibbtc_zap.exchange_underlying(ibbtc_pool, 0, 2, ibbtc_amount, 0)
+    balance_wbtc_after_swap = wbtc.balanceOf(deployer)
 
     assert balance_wbtc_after_swap > balance_wbtc_before_swap
 
     # swap wbtc -> ibbtc
-    wbtc_amount = wBTC.balanceOf(deployer) // 10
+    wbtc_amount = wbtc.balanceOf(deployer) // 10
 
     balance_ibbtc_before_swap = ibbtc.balanceOf(deployer)
-    ibbtc_zap.exchange_underlying(metapool, 2, 0, wbtc_amount, 0)
+    ibbtc_zap.exchange_underlying(ibbtc_pool, 2, 0, wbtc_amount, 0)
     balance_ibbtc_after_swap = ibbtc.balanceOf(deployer)
 
     assert balance_ibbtc_after_swap > balance_ibbtc_before_swap
 
     # swap wbtc -> renbtc
-    wbtc_amount = wBTC.balanceOf(deployer) // 10
+    wbtc_amount = wbtc.balanceOf(deployer) // 10
 
     balance_renbtc_before_swap = renbtc.balanceOf(deployer)
-    ibbtc_zap.exchange_underlying(metapool, 2, 1, wbtc_amount, 0)
+    ibbtc_zap.exchange_underlying(ibbtc_pool, 2, 1, wbtc_amount, 0)
     balance_renbtc_after_swap = renbtc.balanceOf(deployer)
 
     assert balance_renbtc_after_swap > balance_renbtc_before_swap
 
     # swap ibbtc -> wbtc -> ibbtc
-    ibbtc_amount = ibbtc.balanceOf(deployer) // 10
+    balance_ibbtc_before_swap = ibbtc.balanceOf(deployer)
 
-    balance_wbtc_before_swap = wBTC.balanceOf(deployer)
-    ibbtc_zap.exchange_underlying(metapool, 0, 2, ibbtc_amount, 0)
-    balance_wbtc_after_swap = wBTC.balanceOf(deployer)
+    balance_wbtc_before_swap = wbtc.balanceOf(deployer)
+    ibbtc_zap.exchange_underlying(ibbtc_pool, 0, 2, ibbtc_amount, 0)
+    balance_wbtc_after_swap = wbtc.balanceOf(deployer)
 
-    ibbtc_zap.exchange_underlying(metapool, 2, 0, balance_wbtc_after_swap - balance_wbtc_before_swap, 0)
+    ibbtc_zap.exchange_underlying(
+        ibbtc_pool, 2, 0, balance_wbtc_after_swap - balance_wbtc_before_swap, 0
+    )
     balance_ibbtc_after_swap = ibbtc.balanceOf(deployer)
 
-    assert balance_ibbtc_after_swap >= ibbtc_amount * 0.99
-
-# Testing calc_withdraw_one_coin
-def test_calc_withdraw_one_coin(deployer, metapool, ibbtc_zap, ibbtc, renbtc, wBTC, sBTC, wibBTC):
-    initialize(deployer, ibbtc_zap, metapool, ibbtc, renbtc, wBTC, sBTC)
-
-    amount = metapool.balanceOf(deployer) // 10
-    withdraw_amount_ibbtc = ibbtc_zap.calc_withdraw_one_coin(metapool.address, amount, 0, {"from": deployer})
-    withdraw_amount_renbtc = ibbtc_zap.calc_withdraw_one_coin(metapool.address, amount, 1, {"from": deployer})
-    withdraw_amount_wBTC = ibbtc_zap.calc_withdraw_one_coin(metapool.address, amount, 2, {"from": deployer})
-    withdraw_amount_sBTC = ibbtc_zap.calc_withdraw_one_coin(metapool.address, amount, 3, {"from": deployer})
-
-    before_ibbtc_shares = ibbtc.balanceOf(deployer)
-    ibbtc_zap.remove_liquidity_one_coin(metapool, amount, 0, 0, {"from": deployer})
-    after_ibbtc_shares = ibbtc.balanceOf(deployer)
-
-    assert after_ibbtc_shares - before_ibbtc_shares >= withdraw_amount_ibbtc * 0.99
-
-#################### Setup ####################
-
-@pytest.fixture
-def setup(deployer, ibbtc, renbtc, wBTC):
-    metapool = Contract("0xFbdCA68601f835b27790D98bbb8eC7f05FDEaA9B")
-    ibbtc_zap = DepositZapibBTC.deploy({"from": deployer})
-
-    ## Uniswap some tokens here
-    router = Contract("0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F")
-    router.swapExactETHForTokens(
-        0,  ## Mint out
-        ["0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", wBTC.address, ibbtc.address],
-        deployer,
-        9999999999999999,
-        {"from": deployer, "value": 100e18},
+    assert float(balance_ibbtc_after_swap) == approx(
+        balance_ibbtc_before_swap, rel=1e-2
     )
 
-    router.swapExactETHForTokens(
-        0,  ## Mint out
-        ["0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", renbtc.address],
-        deployer,
-        9999999999999999,
-        {"from": deployer, "value": 100e18},
+
+def test_zap_flow(deployer, ibbtc_pool, ibbtc_zap, ibbtc):
+    balance_ibbtc_before = ibbtc.balanceOf(deployer)
+
+    # deposit ibbtc
+    amounts = [ibbtc.balanceOf(deployer) // 2, 0, 0, 0]
+    tx = ibbtc_zap.add_liquidity(ibbtc_pool, amounts, 0, {"from": deployer})
+
+    # remove liquidity in terms of ibbtc
+    ibbtc_zap.remove_liquidity_one_coin(
+        ibbtc_pool, tx.return_value, 0, 0, {"from": deployer}
     )
 
-    router.swapExactETHForTokens(
-        0,  ## Mint out
-        ["0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", wBTC.address],
-        deployer,
-        9999999999999999,
-        {"from": deployer, "value": 200e18},
-    )
+    balance_ibbtc_after = ibbtc.balanceOf(deployer)
 
-    
-    curve_sbtc_pool = Contract("0x7fC77b5c7614E1533320Ea6DDc2Eb61fa00A9714")
-
-    balance_wbtc = wBTC.balanceOf(deployer)
-
-    wBTC.approve(curve_sbtc_pool, 999999999999999999999999999999, {"from": deployer})
-    curve_sbtc_pool.exchange(1, 2, balance_wbtc / 2, 0, {"from": deployer})
-
-    # now the deployer has ibbtc, renbtc, wBTC
-    return DotMap(
-        ibbtc_zap=ibbtc_zap,
-        metapool=metapool
-    )
-
-@pytest.fixture
-def ibbtc_zap(setup):
-    return setup.ibbtc_zap
-
-@pytest.fixture
-def ibbtc():
-    return interface.ERC20("0xc4E15973E6fF2A35cC804c2CF9D2a1b817a8b40F")
-
-@pytest.fixture
-def renbtc():
-    return interface.ERC20("0xEB4C2781e4ebA804CE9a9803C67d0893436bB27D")
-
-@pytest.fixture
-def wBTC():
-    return interface.ERC20("0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599")
-
-@pytest.fixture
-def wibBTC():
-    return Contract("0x8751D4196027d4e6DA63716fA7786B5174F04C15")
-
-@pytest.fixture
-def sBTC():
-    return Contract("0xfE18be6b3Bd88A2D2A7f928d00292E7a9963CfC6")
-
-@pytest.fixture
-def metapool(setup):
-    return setup.metapool
-
-# lp token part of the metapool crvRenWSBTC
-@pytest.fixture
-def lp_token(metapool):
-    return Contract(metapool.coins(1))
-
-@pytest.fixture
-def deployer():
-    yield accounts[0]
-
-@pytest.fixture
-def rando():
-    yield accounts[1]
-
-def approve(deployer, ibbtc_zap, metapool, ibbtc, renbtc, wBTC, sBTC):
-    
-    ibbtc.approve(ibbtc_zap, 999999999999999999999999999999, {"from": deployer})
-    renbtc.approve(ibbtc_zap, 999999999999999999999999999999, {"from": deployer})
-    wBTC.approve(ibbtc_zap, 999999999999999999999999999999, {"from": deployer})
-    sBTC.approve(ibbtc_zap, 999999999999999999999999999999, {"from": deployer})
-    metapool.approve(ibbtc_zap, 999999999999999999999999999999, {"from": deployer})
-
-def approve_rando(rando, ibbtc_zap, metapool, ibbtc, renbtc, wBTC, sBTC):
-    
-    ibbtc.approve(ibbtc_zap, 999999999999999999999999999999, {"from": rando})
-    renbtc.approve(ibbtc_zap, 999999999999999999999999999999, {"from": rando})
-    wBTC.approve(ibbtc_zap, 999999999999999999999999999999, {"from": rando})
-    sBTC.approve(ibbtc_zap, 999999999999999999999999999999, {"from": rando})
-    metapool.approve(ibbtc_zap, 999999999999999999999999999999, {"from": rando})
-
-
-def initialize(deployer, ibbtc_zap, metapool, ibbtc, renbtc, wBTC, sBTC):
-    approve(deployer, ibbtc_zap, metapool, ibbtc, renbtc, wBTC, sBTC)
-
-    # initialize pool by depositing all coins in equal proportions
-    amounts = [ibbtc.balanceOf(deployer) // 2, renbtc.balanceOf(deployer) // 2, renbtc.balanceOf(deployer) // 2, sBTC.balanceOf(deployer) // 2]
-    ibbtc_zap.add_liquidity(metapool.address, amounts, 0, {"from": deployer})
+    assert float(balance_ibbtc_after) == approx(balance_ibbtc_before, rel=1e-2)
