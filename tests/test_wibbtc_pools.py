134d2120d0d642fd6ce1c4253b649d939069cf06
Ayush Shukla <shuklaayush247@gmail.com>
2021-11-08 20:07:42 +0530

    test: add playground for ibbtc pools

--- tests/test_wibbtc_pools.py
+++ tests/test_wibbtc_pools.py
@@ -1,18 +1,24 @@
 import pytest
 
-from conftest import MAX_UINT256, deployer
+from brownie import MetaBTC, MetaBTCBalances
+from conftest import MAX_UINT256, weth
+
+
+@pytest.fixture(autouse=True, params=[MetaBTC, MetaBTCBalances])
+def ibbtc_pool(request, wibbtc, deployer):
+    pool = request.param.deploy({"from": deployer})
+    pool.initialize("wibBTC", "wibBTC", wibbtc, 1e18, 100, 4_000_000, {"from": deployer})
+    yield pool
 
 
 @pytest.fixture(autouse=True)
-def ibbtc_pool(MetaBTC, wibbtc, deployer):
-    yield MetaBTC.deploy(
-        "ibBTC", "ibbtc", wibbtc, 1e18, 100, 4_000_000, {"from": deployer}
-    )
+def ibbtc_zap(DepositZapibBTC, deployer):
+    yield DepositZapibBTC.deploy({"from": deployer})
 
 
 @pytest.fixture(autouse=True)
 def core(Core, deployer):
-    yield Core.deploy(1.2e18, {"from": deployer})
+    yield Core.deploy(1e18, {"from": deployer})
 
 
 @pytest.fixture(autouse=True)
@@ -23,7 +29,7 @@ def set_core(wibbtc, deployer, core):
 
 
 @pytest.fixture(autouse=True)
-def seed_pool(wibbtc, ibbtc_zap, ibbtc_pool, deployer, renbtc, wbtc, sbtc):
+def seed_pool(ibbtc_zap, ibbtc_pool, deployer, wibbtc, renbtc, wbtc, sbtc):
     ibbtc_pool.approve(ibbtc_zap, MAX_UINT256, {"from": deployer})
 
     # Initialize pool to 1: 1 wibbtc/sbtcCrv peg
@@ -33,48 +39,52 @@ def seed_pool(wibbtc, ibbtc_zap, ibbtc_pool, deployer, renbtc, wbtc, sbtc):
         10 ** wbtc.decimals() // 3,  # wbtc
         10 ** sbtc.decimals() // 3,  # sbtc
     ]
-    print(amounts)
     ibbtc_zap.add_liquidity(ibbtc_pool, amounts, 0, {"from": deployer})
 
 
-def test_pool_rebalance(ibbtc_pool, wibbtc, sbtc_pool, core, deployer, ibbtc_zap):
+def test_pool_rebalance(wibbtc, ibbtc_pool, sbtc_pool, core, deployer, ibbtc_zap):
     print("-" * 80)
+    print(f"PPS: {wibbtc.pricePerShare()}")
     print(f"ibBTC balance: {wibbtc.balanceToShares(wibbtc.balanceOf(ibbtc_pool))}")
     print(
         f"sbtcCrv underlying balance: {sbtc_pool.get_virtual_price() * ibbtc_pool.balances(1) // int(1e18)}"
     )
 
-    print("-" * 80)
-    print(f"wibBTC balance: {wibbtc.balanceOf(ibbtc_pool)}")
+    print(f"wibBTC balance: {ibbtc_pool.balances(0)}")
     print(f"sbtcCrv balance: {ibbtc_pool.balances(1)}")
 
-    print("-" * 80)
     print(f"Virtual price: {ibbtc_pool.get_virtual_price()}")
     print("-" * 80)
 
     # Update pps
-    core.setPricePerShare(1.4e18)
+    core.setPricePerShare(1.2e18)
     wibbtc.updatePricePerShare({"from": deployer})
 
     print("-" * 80)
+    print(f"PPS: {wibbtc.pricePerShare()}")
     print(f"ibBTC balance: {wibbtc.balanceToShares(ibbtc_pool.balances(0))}")
     print(
         f"sbtcCrv underlying balance: {sbtc_pool.get_virtual_price() * ibbtc_pool.balances(1) // int(1e18)}"
     )
 
-    print("-" * 80)
     print(f"wibBTC balance: {ibbtc_pool.balances(0)}")
     print(f"sbtcCrv balance: {ibbtc_pool.balances(1)}")
 
-    print("-" * 80)
     print(f"Virtual price: {ibbtc_pool.get_virtual_price()}")
     print("-" * 80)
 
-    ibbtc_zap.add_liquidity(ibbtc_pool, [0, 0.4e18, 0, 0], 0, {"from": deployer})
+    # Add some renbtc
+    ibbtc_zap.add_liquidity(ibbtc_pool, [0, 0.4e8, 0, 0], 0, {"from": deployer})
     print("-" * 80)
+    print(f"PPS: {wibbtc.pricePerShare()}")
+
+    print(f"ibBTC balance: {wibbtc.balanceToShares(ibbtc_pool.balances(0))}")
+    print(
+        f"sbtcCrv underlying balance: {sbtc_pool.get_virtual_price() * ibbtc_pool.balances(1) // int(1e18)}"
+    )
+
     print(f"wibBTC balance: {ibbtc_pool.balances(0)}")
     print(f"sbtcCrv balance: {ibbtc_pool.balances(1)}")
 
-    print("-" * 80)
     print(f"Virtual price: {ibbtc_pool.get_virtual_price()}")
     print("-" * 80)
